19d8cddb889e794ef7bafcdbf608032f1bc4e55a
rifqiadrianto007 <232410103042@mail.unej.ac.id>
2026-02-06 20:57:34 +0700

    dashboard page update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,12 +3,14 @@ import Navbar from "../components/Navbar"
 export default function Dashboard() {
 
     return (
-        <div>
+        <div className="min-h-screen flex flex-col">
 
             <Navbar />
 
-            <div className="p-6 max-w-5xl mx-auto w-full">
-                Dashboard Page
+            <div className="flex-1 flex items-center justify-center p-6">
+                <div className="max-w-5xl mx-auto w-full text-center text-4xl font-bold">
+                    Dashboard Page
+                </div>
             </div>
 
         </div>
